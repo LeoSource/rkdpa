f2cd52c234f18981ea724cabe5ca9173469c4a25
zxliao <zhixiangleo@163.com>
2021-03-11 17:13:54 +0800

    feat：新增RRT算法平面两连杆避障demo

---
+++ src/TestRRT.m
@@ -0,0 +1,37 @@
+clear 
+close all
+clc
+
+dim = 2;
+step_size = [5]*pi/180;
+random_world = 0;
+show_output = 1;
+num_of_runs = 1;
+
+for idx=1:size(step_size,1)
+    segmentLength = step_size(idx);
+    
+    time = 0;
+    avg_its = 0;
+    avg_path = 0;
+    for i=1:num_of_runs
+        [n_its, path_n, run_time] =  RRTconnect(dim,segmentLength,random_world,show_output);
+        time = time + run_time;
+        avg_its = avg_its + n_its;
+        avg_path = avg_path + path_n;            
+    end
+    
+    str1 = ['The time taken by RRT-Connect for ', num2str(num_of_runs), ' runs is ', num2str(time)];
+    str2 = ['The averagae time taken by RRT-Connect for each run is ', num2str(time/num_of_runs)];
+    str3 = ['The averagae number of states explored by RRT-Connect for each run is ', num2str(avg_its/num_of_runs)];%random times
+    str4 = ['The averagae number of state in Path by RRT-Connect for each run is ', num2str(avg_path/num_of_runs)];        
+    disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+    disp(str1);
+    disp(str2);
+    disp(str3);
+    disp(str4);
+    disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+end
+
+
+
