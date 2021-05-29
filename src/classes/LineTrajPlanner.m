4c5a21d88c3d1693d9f819919c5c1667d96588c9
zxliao <zhixiangleo@163.com>
2021-05-29 14:46:17 +0800

    fix：rpy角规划采用单一变量规划，简化代码

--- src/classes/LineTrajPlanner.m
+++ src/classes/LineTrajPlanner.m
@@ -10,60 +10,54 @@ classdef LineTrajPlanner < handle
         pos_initial
         rpy_initial
         pos_dir
-        tf
+        rot_dir
+        tf_pos
+        tf_rot
         pos_uplanner
         rot_uplanner
-    end
-    properties
-        roll_planner
-        pitch_planner
-        yaw_planner
+        
         option
     end
 
     methods
-        function obj = LineTrajPlanner(pos0, posn,line_vmax,line_amax,...
-                                        rpy0,rpyn,ang_vmax,ang_amax,opt)
+        function obj = LineTrajPlanner(pos0, posn,line_vmax,line_amax,pduration,pvel_cons,...
+                                        rpy0,rpyn,ang_vmax,ang_amax,rduration,rvel_cons,opt)
             obj.option = opt;
             obj.pos_initial = pos0;
             obj.rpy_initial = rpy0;
             if strcmp(opt,'both')
-                obj.InitPosPlanner(pos0,posn,line_vmax,line_amax);
-                obj.InitRotPlanner(rpy0,rpyn,ang_vmax,ang_amax);
-                obj.tf = max([obj.pos_uplanner.tf, obj.pitch_planner.tf,...
-                            obj.yaw_planner.tf, obj.roll_planner.tf]);
-                obj.InitPosPlanner(pos0,posn,line_vmax,line_amax,obj.tf);
-                obj.InitRotPlanner(rpy0,rpyn,ang_vmax,ang_amax,obj.tf);
+                obj.InitPosPlanner(pos0,posn,line_vmax,line_amax,pduration,pvel_cons);
+                obj.InitRotPlanner(rpy0,rpyn,ang_vmax,ang_amax,rduration,rvel_cons);
+                obj.tf_pos = obj.pos_uplanner.tf;
+                obj.tf_rot = obj.rot_uplanner.tf;
             elseif strcmp(opt, 'pos')
-                obj.InitPosPlanner(pos0,posn,line_vmax,line_amax);
-                obj.tf = obj.pos_uplanner.tf;
+                obj.InitPosPlanner(pos0,posn,line_vmax,line_amax,pduration,pvel_cons);
+                obj.tf_pos = obj.pos_uplanner.tf;
             elseif strcmp(opt, 'rot')
-                obj.InitRotPlanner(rpy0,rpyn,ang_vmax,ang_amax);
-                obj.tf = max([obj.roll_planner.tf,obj.pitch_planner.tf,obj.yaw_planner.tf]);
+                obj.InitRotPlanner(rpy0,rpyn,ang_vmax,ang_amax,rduration,rvel_cons);
+                obj.tf_rot = max([obj.roll_planner.tf,obj.pitch_planner.tf,obj.yaw_planner.tf]);
             else
                 error('error line option');
             end
         end
 
-        function InitPosPlanner(obj, pos0, posn, line_vmax, line_amax, tf)
+        function InitPosPlanner(obj, pos0, posn, line_vmax, line_amax, tf, vel_cons)
             line_len = norm(posn-pos0);
             obj.pos_dir = (posn-pos0)/line_len;
-            if nargin==5
-                obj.pos_uplanner = LspbTrajPlanner([0,line_len], line_vmax, line_amax);
-            elseif nargin==6
+            if isempty(tf)
+                obj.pos_uplanner = LspbTrajPlanner([0,line_len], line_vmax, line_amax,[],vel_cons);
+            else
                 obj.pos_uplanner = LspbTrajPlanner([0,line_len], line_vmax, line_amax,tf);
             end
         end
 
-        function InitRotPlanner(obj, rpy0, rpyn, ang_vmax, ang_amax, tf)
-            if nargin==5
-                obj.roll_planner = LspbTrajPlanner([rpy0(1), rpyn(1)],ang_vmax(1),ang_amax(1));
-                obj.pitch_planner = LspbTrajPlanner([rpy0(2), rpyn(2)],ang_vmax(2),ang_amax(2));
-                obj.yaw_planner = LspbTrajPlanner([rpy0(3),rpyn(3)],ang_vmax(3),ang_amax(3));
-            elseif nargin==6
-                obj.roll_planner = LspbTrajPlanner([rpy0(1), rpyn(1)],ang_vmax(1),ang_amax(1),tf);
-                obj.pitch_planner = LspbTrajPlanner([rpy0(2), rpyn(2)],ang_vmax(2),ang_amax(2),tf);
-                obj.yaw_planner = LspbTrajPlanner([rpy0(3),rpyn(3)],ang_vmax(3),ang_amax(3),tf);
+        function InitRotPlanner(obj, rpy0, rpyn, ang_vmax, ang_amax, tf,vel_cons)
+            rpy_len = norm(rpyn-rpy0);
+            obj.rot_dir = (rpyn-rpy0)/rpy_len;
+            if isempty(tf)
+                obj.rot_uplanner = LspbTrajPlanner([0,rpy_len], ang_vmax, ang_amax, [], vel_cons);
+            else
+                obj.rot_uplanner = LspbTrajPlanner([0,rpy_len], ang_vmax, ang_amax, tf);
             end
         end
 
@@ -86,10 +80,8 @@ classdef LineTrajPlanner < handle
         end
 
         function rpy = GenerateRot(obj,t)
-            rpy = zeros(3,1);
-            rpy(1) = obj.roll_planner.GenerateMotion(t);
-            rpy(2) = obj.pitch_planner.GenerateMotion(t);
-            rpy(3) = obj.yaw_planner.GenerateMotion(t);
+            [up,~,~] = obj.rot_uplanner.GenerateMotion(t);
+            rpy = obj.rpy_initial+up*obj.rot_dir;
         end
 
         function [pos,rpy] = GeneratePath(obj, dt)
