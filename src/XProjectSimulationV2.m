2362ad5f613ad9c9e9baf2575908f1216fbdf415
zxliao <zhixiangleo@163.com>
2021-07-26 20:40:08 +0800

    fix：修复多段轨迹拼接时关节位置突跳问题

--- src/XProjectSimulationV2.m
+++ src/XProjectSimulationV2.m
@@ -34,37 +34,24 @@ switch simu_mode
     case 'mirror'
 %% simulation mirror task
 q0 = [0,0,0,0,-pi/2,0]';
+q1 = [0,145,-240,90,-90,0]'*pi/180;
+q2 = [0,-35,50,-100,-90,0]'*pi/180;
 dt = 0.01;
-comparision = 0;
+compare_cpp = 0;
+compare_plan = 1;
 pose0 = rbt.fkine(q0);
 pos0 = pose0.t;
 rpy0 = tr2rpy(pose0,'xyz');
-% ctraj = CartesianBasePlanner([pos0;rpy0'], 1);
 pos1 = [0.5,0,1]'; pos2 = [0.65,0,1]'; pos3 = [0.65,0,0.6]';
 rpy1 = [0,pi/2,pi/2]'; rpy2 = [0,pi/2,pi/2]'; rpy3 = [0,pi/2,pi/2]';
-taskplanner = TaskTrajPlanner(rbt,q0);
-taskplanner.AddTraj([[pos0;rpy0'],[pos1;rpy1],[pos2;rpy2],[pos3;rpy3]], 'cartesian', 0);
-[cpos,cvel,cacc,jpos,jvel,jacc] = taskplanner.GenerateBothTraj(0.01);
-% ctraj.AddPosRPY([[pos1;rpy1],[pos2;rpy2],[pos3;rpy3]]);
-% [pos,~,~,rpy,~,~] = ctraj.GenerateTraj(dt);
 
-% sim_q = []; sim_pos = []; pre_q = q0;
-% for idx=1:size(pos,2)
-%     cmd_pose = SE3.rpy(180/pi*rpy(:,idx)','xyz');
-%     cmd_pose.t = pos(:,idx);
-%     rot_mount = cmd_pose.R*(pose_tool.R)';
-%     pos_mount = cmd_pose.t-rot_mount*pose_tool.t;
-%     pose_mount = rt2tr(rot_mount,pos_mount);
-%     tmp_q = rbt.ikine(pose_mount, 'q0',pre_q', 'tol',1e-5)';
-%     sim_q = [sim_q, tmp_q];
-%     pose_tmp = rbt.fkine(tmp_q)*pose_tool;
-%     sim_pos = [sim_pos, pose_tmp.t];
-%     pre_q = tmp_q;
-% end
-
+taskplanner = TaskTrajPlanner(rbt,q0,compare_plan);
+taskplanner.AddTraj([q1,q2], 'joint', 1);
+taskplanner.AddTraj([[pos1;rpy1],[pos2;rpy2],[pos3;rpy3]], 'cartesian', 0);
+[cpos,cvel,cacc,jpos,jvel,jacc,cpos_sim] = taskplanner.GenerateBothTraj(0.01);
 %% plot and compare with cpp data
 figure
-plot2(cpos(1:3,:)', 'r--'); hold on; %plot2(sim_pos', 'k');
+plot2(cpos(1:3,:)', 'r--'); hold on; plot2(cpos_sim', 'k');
 plot2([pos1,pos2,pos3]', 'bo'); axis equal; hold off;
 grid on; xlabel('X(m)'); ylabel('Y(m)'); zlabel('Z(m)');
 
@@ -73,7 +60,7 @@ figure
 plot(t,jpos(1,:),'-', t, jpos(2,:), '--', t, jpos(3,:), '-.', t, jpos(4,:), ':', t, jpos(5,:), '-', t,jpos(6,:),'k');
 grid on; title('joint position'); legend('q1', 'q2', 'q3', 'q4', 'q5', 'q6');
 
-if comparision
+if compare_cpp
     q_cpp = load('./data/mirrortask_jpos1.csv');
     q_cpp = reshape(q_cpp, rbt.n, []);
     tt = g_cycle_time*[0:size(q_cpp,2)-1];
--- src/classes/TaskTrajPlanner.m
+++ src/classes/TaskTrajPlanner.m
@@ -8,36 +8,69 @@ classdef TaskTrajPlanner < handle
 
         robot
         pre_q
+        pre_trajq
+        pre_trajpose
+        compare_plan
     end
     
     methods
-        function obj = TaskTrajPlanner(rbt_model,q0)
+        function obj = TaskTrajPlanner(rbt_model,q0,compare_plan)
             obj.robot = rbt_model;
             obj.pre_q = q0;
+            obj.compare_plan = compare_plan;
             obj.ntraj = 0;
+            obj.pre_trajq = q0;
+            obj.pre_trajpose = obj.robot.fkine(q0);
         end
         
         function AddTraj(obj, via_pos, traj_type, traj_opt)
             switch traj_type
-                case 'cartesian'
-                    cplanner = CartesianBasePlanner(via_pos(:,1), traj_opt);
-                    cplanner.AddPosRPY(via_pos(:,2:end));
-                    obj.ntraj = obj.ntraj+1;
-                    obj.segplanner{obj.ntraj} = cplanner;
-                    obj.traj_type{obj.ntraj} = 'cartesian';
-                case 'joint'
-                    jplanner = JointPlanner(via_pos(:,1), traj_opt);
-                    jplanner.AddJntPos(via_pos(:,2:end));
-                    obj.ntraj = obj.ntraj+1;
-                    obj.segplanner{obj.ntraj} = jplanner;
-                    obj.traj_type{obj.ntraj} = 'joint';
+            case 'cartesian'
+                pos0 = obj.pre_trajpose.t;
+                rpy0 = tr2rpy(obj.pre_trajpose,'xyz')';
+                cplanner = CartesianBasePlanner([pos0;rpy0], traj_opt);
+                cplanner.AddPosRPY(via_pos);
+                obj.ntraj = obj.ntraj+1;
+                obj.segplanner{obj.ntraj} = cplanner;
+                obj.traj_type{obj.ntraj} = 'cartesian';
+                obj.pre_trajpose  = SE3.rpy(180/pi*via_pos(4:6,end)', 'xyz');
+                obj.pre_trajpose.t = via_pos(1:3,end);
+                obj.pre_trajq = obj.robot.ikine(obj.pre_trajpose,'q0',obj.pre_trajq','tol',1e-5)';
+            case 'joint'
+                jplanner = JointPlanner(obj.pre_trajq, traj_opt);
+                jplanner.AddJntPos(via_pos);
+                obj.ntraj = obj.ntraj+1;
+                obj.segplanner{obj.ntraj} = jplanner;
+                obj.traj_type{obj.ntraj} = 'joint';
+                obj.pre_trajq = via_pos(:,end);
+                obj.pre_trajpose = obj.robot.fkine(obj.pre_trajq);
             end
         end
 
 
-        function [cpos,cvel,cacc,jpos,jvel,jacc] = GenerateBothTraj(obj,dt)
-            [cpos,cvel,cacc] = obj.GenerateCartTraj(dt);
-            [jpos,jvel,jacc] = obj.GenerateJointTraj(dt);
+        function [cpos,cvel,cacc,jpos,jvel,jacc,cpos_sim] = GenerateBothTraj(obj,dt)
+            cpos=[]; cvel=[]; cacc=[];
+            jpos=[]; jvel=[]; jacc=[];
+            cpos_sim=[];
+            for traj_idx=1:obj.ntraj
+                switch obj.traj_type{traj_idx}
+                case 'cartesian'
+                    [p,vp,ap,r,vr,ar] = obj.segplanner{traj_idx}.GenerateTraj(dt);
+                    pos_tmp = [p;r]; vel_tmp = [vp;vr]; acc_tmp = [ap;ar];
+                    cpos = [cpos,pos_tmp]; cvel = [cvel,vel_tmp]; cacc = [cacc,acc_tmp];
+                    [jp,jv,ja,cp_sim] = obj.Transform2Joint(pos_tmp,vel_tmp,acc_tmp);
+                    jpos = [jpos,jp]; jvel = [jvel, jv]; jacc = [jacc,ja];
+                    cpos_sim=[cpos_sim,cp_sim];
+                case 'joint'
+                    [jp,jv,ja] = obj.segplanner{traj_idx}.GenerateTraj(dt);
+                    jpos = [jpos,jp]; jvel = [jvel, jv]; jacc = [jacc,ja];
+%                     [p,v,a] = obj.Transform2Cart(jp,jv,ja);
+%                     cpos = [cpos, p]; cvel = [cvel, v]; cacc = [cacc, a];
+%                     if obj.compare_plan
+%                         cpos_sim = [cpos_sim,p(1:3,:)];
+%                     end
+                end
+            end
         end
 
         function [cpos,cvel,cacc] = GenerateCartTraj(obj, dt)
@@ -89,8 +122,8 @@ classdef TaskTrajPlanner < handle
             end
         end
 
-        function [jpos,jvel,jacc] = Transform2Joint(obj,cp,cv,ca)
-            jpos=[]; jvel=[]; jacc=[];
+        function [jpos,jvel,jacc,cpos_sim] = Transform2Joint(obj,cp,cv,ca)
+            jpos=[]; jvel=[]; jacc=[]; cpos_sim=[];
             np = size(cp,2);
             for idx=1:np
                 cmd_pose = SE3.rpy(180/pi*cp(4:6,idx)','xyz');
@@ -102,6 +135,10 @@ classdef TaskTrajPlanner < handle
                 qdd = jaco\(ca(:,idx)-jaco_dot);
                 obj.pre_q = q;
                 jpos=[jpos,q]; jvel=[jvel,qd]; jacc=[jacc,qdd];
+                if obj.compare_plan
+                    cp_sim = obj.robot.fkine(q).t;
+                    cpos_sim = [cpos_sim, cp_sim];
+                end
             end
         end
 
