cac715fc7b23a7e576c81234903c688357925d31
zxliao <zhixiangleo@163.com>
2021-08-11 09:40:09 +0800

    feat：增加台面姿态规划功能

--- src/tools/CalcViapos.m
+++ src/tools/CalcViapos.m
@@ -1,12 +1,12 @@
-function via_pos = CalcViapos(vision_pos, type)
+function via_posrpy = CalcViapos(vision_pos, type)
 
     npos = size(vision_pos,2);
     if strcmp(type, 'toilet')
         center = mean(vision_pos, 2);
         rot_center = [0, 1, 0; 1, 0, 0; 0, 0, -1];
         rpy = tr2rpy(rot_center, 'xyz');
-        via_pos(:,1) = [center; rpy'];
-        theta = 20*pi/180;
+        via_posrpy(:,1) = [center; rpy'];
+        theta = 20*pi/180;%angle of vertical plane
         for idx=1:npos
             pos_tmp = vision_pos(:,idx);
             len = norm(center-pos_tmp);
@@ -20,8 +20,38 @@ function via_pos = CalcViapos(vision_pos, type)
             y0 = cross(z0,x0);
             rot_mat = [x0, y0, z0];
             rpy = tr2rpy(rot_mat, 'xyz');
-            via_pos(:,idx+1) = [pos_tmp; rpy'];
+            via_posrpy(:,idx+1) = [pos_tmp; rpy'];
         end
+    elseif strcmp(type,'table')
+        theta = 40*pi/180;%angle of horizontal plane
+        origin = 0.5*(vision_pos(:,1)+vision_pos(:,2));
+        len = norm(origin-vision_pos(:,3));
+        origin(3) = origin(3)+len*tan(theta);
+        interval = 0.02;
+        step_vec1 = vision_pos(:,3)-vision_pos(:,4);
+        step_vec2 = vision_pos(:,2)-vision_pos(:,1);
+        start_pos1 = vision_pos(:,4);
+        start_pos2 = vision_pos(:,1);
+        cycle_num = round(norm(step_vec1)/interval)+1;
+%         numvp = 2*cycle_num;
+        step_size = norm(step_vec1)/(cycle_num-1);
+        step_vec1 = step_vec1/norm(step_vec1);
+        step_vec2 = step_vec2/norm(step_vec2);
+        for idx=1:cycle_num
+            pos1 = start_pos1+(idx-1)*step_size*step_vec1;
+            pos2 = start_pos2+(idx-1)*step_size*step_vec2;
+            z0 = pos1-origin;
+            v1 = -z0;
+            v2 = 0.5*(vision_pos(:,1)+vision_pos(:,2))-pos1;
+            x0 = cross(v1,v2);
+            y0 = cross(z0,x0);
+            rot_mat = [x0/norm(x0), y0/norm(y0), z0/norm(z0)];
+            rpy = tr2rpy(rot_mat, 'xyz');
+            via_posrpy(:,2*idx-1) = [pos1; rpy'];
+            via_posrpy(:,2*idx) = [pos2; rpy'];
+        end
+    elseif strcmp(type, 'washbowl')
+
     end
 
 end
