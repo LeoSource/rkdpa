8db9edd27df9a4b72d1fa7e344ee4b6762a94f71
zxliao <zhixiangleo@163.com>
2021-11-08 13:54:36 +0800

    fix：简化数据处理方法，优化重力辨识流程

--- src/TestGravityIden.m
+++ src/TestGravityIden.m
@@ -6,126 +6,15 @@ addpath('classes');
 addpath(genpath('tools'));
 addpath('gravity')
 dt = 0.005;
-%% robot gravity identification
-grav_regressor = RobotGravityIden;
-
-reg_grav = []; tau_grav = []; jpos = [];
-jpos5 = []; tau5 = [];
-for idx=1:16
-    data_file = ['./gravity/joint5_data',num2str(idx),'.mat'];
-    jdata = load(data_file);
-    jpos5 = [jpos5; jdata.jpos_iden];
-    tau5 = [tau5; jdata.tor_iden];
-    clear jdata
-end
-tau_grav = [tau_grav; tau5];
-jpos = [jpos; jpos5];
-for idx=1:length(tau5)
-    reg_tmp = grav_regressor.CalcRegressorJoint5(jpos5(idx,:));
-    reg_grav = [reg_grav; reg_tmp];
-end
-
-jpos4 = []; tau4 = [];
-for idx=1:9
-    data_file = ['./gravity/joint4_data',num2str(idx),'.mat'];
-    jdata = load(data_file);
-    jpos4 = [jpos4; jdata.jpos_iden];
-    tau4 = [tau4; jdata.tor_iden];
-    clear jdata
-end
-tau_grav = [tau_grav; tau4];
-jpos = [jpos; jpos4];
-for idx=1:length(tau4)
-    reg_tmp = grav_regressor.CalcRegressorJoint4(jpos4(idx,:));
-    reg_grav = [reg_grav; reg_tmp];
-end
-
-jpos3 = []; tau3 = [];
-for idx=1:4
-    data_file = ['./gravity/joint3_data',num2str(idx),'.mat'];
-    jdata = load(data_file);
-    jpos3 = [jpos3; jdata.jpos_iden];
-    tau3 = [tau3; jdata.tor_iden];
-    clear jdata
-end
-tau_grav = [tau_grav; tau3];
-jpos = [jpos; jpos3];
-for idx=1:length(tau3)
-    reg_tmp = grav_regressor.CalcRegressorJoint3(jpos3(idx,:));
-    reg_grav = [reg_grav; reg_tmp];
-end
-
-jpos2 = []; tau2 = [];
-for idx=1:6
-    data_file = ['./gravity/joint2_data',num2str(idx),'.mat'];
-    jdata = load(data_file);
-    jpos2 = [jpos2; jdata.jpos_iden];
-    tau2 = [tau2; jdata.tor_iden];
-    clear jdata
-end
-tau_grav = [tau_grav; tau2];
-jpos = [jpos; jpos2];
-for idx=1:length(tau2)
-    reg_tmp = grav_regressor.CalcRegressorJoint2(jpos2(idx,:));
-    reg_grav = [reg_grav; reg_tmp];
-end
-
-grav_regressor.barycenter_params = pinv(reg_grav)*tau_grav;
-
-tau_val = [];
-for idx=1:length(tau3)
-    tau_tmp = grav_regressor.CalcGravJoint3(jpos3(idx,:));
-    tau_val = [tau_val; tau_tmp];
-end
-plot(tau3,'r'); grid on; hold on; plot(tau_val, 'k');
-legend('identification', 'validation');
-
 
 %% load joint test data
 nj = 6;
-td = load('./data./joint2/test_data_1106_112629.csv');
+td = load('./data/test_data_1106_105529.csv');
 jpos_idx = 1; jvel_idx = 2; jtor_idx = 3;
 jpos = td(:,1:jpos_idx*nj);
 jvel = td(:,nj+1:jvel_idx*nj);
 jtor = td(:,2*nj+1:jtor_idx*nj);
 t = 0:dt:dt*(size(td,1)-1);
-
-%% process joint position and torque
-test_joint_idx = 2;
-jpos_iden = [];
-record_value = [-0.7, 0.7];
-eps = 4e-4;
-start_idx = find(jpos(:,test_joint_idx)>record_value(1)-0.5*eps & jpos(:,test_joint_idx)<record_value(1)+0.5*eps);
-stop_idx = find(jpos(:,test_joint_idx)>record_value(2)-0.5*eps & jpos(:,test_joint_idx)<record_value(2)+0.5*eps);
-if length(start_idx) ~=2  || length(stop_idx) ~=2
-    error('rescop1');
-end
-start_idx(2) = start_idx(2)+1;
-if abs(stop_idx(1)-start_idx(1)) ~= abs(stop_idx(2)-start_idx(2))
-    error('rescope2');
-end
-sparse_value = 5;
-jpos_iden = [jpos_iden; jpos(start_idx(1):stop_idx(1),:)];
-jpos_iden = jpos_iden(1:sparse_value:end,:);
-tor1 = jtor(start_idx(1):stop_idx(1),test_joint_idx);
-tor2_tmp = jtor(stop_idx(2):start_idx(2),test_joint_idx);
-tor2 = fliplr(tor2_tmp');
-tor2 = tor2';
-tor_grav = 0.5*(tor1+tor2);
-tor_iden = tor_grav(1:sparse_value:end);
-
-fs = 200;
-N= 2;
-fc = 5;
-[Btorque, Atorque] = butter(N, fc/(fs/2));
-tor = filtfilt(Btorque, Atorque, jtor(:,test_joint_idx));
-
-figure
-plot(t, jtor(:,test_joint_idx), t, tor); grid on;
-figure
-plot(tor1); grid on; hold on; plot(tor2); hold off;
-save('./gravity/joint2_data6.mat', 'jpos_iden', 'tor_iden');
-
 %% plot joint data
 plot_jdata = 0;
 if plot_jdata
@@ -151,6 +40,6 @@ if plot_jdata
     end
     hold off; legend;
 end
-
-
-
+%% robot gravity identification
+grav_iden = RobotGravityIden;
+grav_iden.Identificatino(jpos,jtor);
--- src/gravity/RobotGravityIden.m
+++ src/gravity/RobotGravityIden.m
@@ -1,10 +1,10 @@
 classdef RobotGravityIden < handle
     
     properties
-        barycenter_j5 %% l5x,l5y,l6x,l6y,l6z,m6
-        barycenter_j4 %% l4x,l4y,l5x,l5y,l5z,m5,l6x,l6y,l6z,m6
-        barycenter_j3 %% l3x,l3y,l4x,l4y,m4,l5x,l5y,l5z,m5,l6x,l6y,l6z,m6
-        barycenter_j2 %% l2x,l2y,l3x,l3y,m3,l4x,l4y,m4,l5x,l5y,l5z,m5,l6x,l6y,l6z,m6
+%         barycenter_j5 %% l5x,l5y,l6x,l6y,l6z,m6
+%         barycenter_j4 %% l4x,l4y,l5x,l5y,l5z,m5,l6x,l6y,l6z,m6
+%         barycenter_j3 %% l3x,l3y,l4x,l4y,m4,l5x,l5y,l5z,m5,l6x,l6y,l6z,m6
+%         barycenter_j2 %% l2x,l2y,l3x,l3y,m3,l4x,l4y,m4,l5x,l5y,l5z,m5,l6x,l6y,l6z,m6
         barycenter_params
     end
     
@@ -12,6 +12,78 @@ classdef RobotGravityIden < handle
     methods
         function obj = RobotGravityIden()
             
+        end
+        
+        function Identificatino(obj,jpos,jtau)
+            [jpos_grav,jtau_grav] = obj.ProcessJointData(jpos,jtau);
+            %%gravity identification%%
+            sparse_value = 5;
+            jpos_iden = jpos_grav(1:sparse_value:end,:);
+            jtau_iden_tmp = jtau_grav(1:sparse_value:end,:);
+            reg_mat = []; jtau_iden = [];
+            for idx=1:size(jpos_iden,1)
+                reg_tmp = obj.CalcRegMat(jpos_iden(idx,:));
+                reg_mat = [reg_mat;reg_tmp];
+                jtau_iden = [jtau_iden; jtau_iden_tmp(idx,2:5)'];
+            end
+            obj.barycenter_params = pinv(reg_mat)*jtau_iden;
+            tau_val = [];
+            for idx=1:size(jpos_grav,1)
+                tau_tmp = obj.GenerateGravTau(jpos_grav(idx,:));
+                tau_val = [tau_val;tau_tmp'];
+            end
+            figure
+            for idx=2:5
+                subplot(2,2,idx-1)
+                plot(jtau_grav(:,idx),'r'); grid on; hold on;
+                plot(tau_val(:,idx),'k'); hold off;
+%                 legend('identification','validation');
+                title(['joint',num2str(idx),' gravity validation']);
+            end
+        end
+        
+        function [jpos_grav,jtau_grav] = ProcessJointData(obj,jpos,jtau)
+            %%process identification data%%
+            record_value = [-0.87,0.87];
+            eps = 1e-5;
+            start_idx = find(jpos(:,2)>record_value(1)-5*eps & jpos(:,2)<record_value(1)+4*eps);
+            stop_idx = find(jpos(:,2)>record_value(2)-4*eps & jpos(:,2)<record_value(2)+4*eps);
+            if length(start_idx) ~=2  || length(stop_idx) ~=2
+                error('rescop1');
+            end
+            start_idx(2) = start_idx(2)+2;
+            if abs(stop_idx(1)-start_idx(1)) ~= abs(stop_idx(2)-start_idx(2))
+                error('rescope2');
+            end
+            jpos_grav = jpos(start_idx(1):stop_idx(1),:);
+            tau1 = jtau(start_idx(1):stop_idx(1),:);
+            tau2_tmp = jtau(stop_idx(2):start_idx(2),:);
+            tau2 = fliplr(tau2_tmp');
+            tau2 = tau2';
+            jtau_grav = 0.5*(tau1+tau2);
+            %%filter joint torque data%%
+            fs = 200;
+            N = 2;
+            fc = 5;
+            [Btorque, Atorque] = butter(N, fc/(fs/2));
+            for idx=1:size(jtau_grav,2)
+                jtau_grav(:,idx) = filtfilt(Btorque, Atorque, jtau_grav(:,idx));
+            end
+            figure
+            for idx=2:5
+                subplot(2,2,idx-1)
+                plot(tau1(:,idx)); grid on; hold on;
+                plot(tau2(:,idx)); plot(jtau_grav(:,idx)); hold off;
+                title(['joint',num2str(idx),' gravity']);
+            end
+        end
+
+        function reg_mat = CalcRegMat(obj,jpos)
+            reg_mat = zeros(4,16);
+            reg_mat(1,:) = obj.CalcRegressorJoint2(jpos);
+            reg_mat(2,:) = obj.CalcRegressorJoint3(jpos);
+            reg_mat(3,:) = obj.CalcRegressorJoint4(jpos);
+            reg_mat(4,:) = obj.CalcRegressorJoint5(jpos);
         end
     
         function reg_vec = CalcRegressorJoint5(obj,q)
@@ -55,7 +127,8 @@ classdef RobotGravityIden < handle
             reg_vec(13) = -9.81*sin(q6)*cos(q2 + q3 + q4) - 9.81*sin(q2 + q3 + q4)*cos(q5)*cos(q6);
             reg_vec(14) = 9.81*sin(q6)*sin(q2 + q3 + q4)*cos(q5) - 9.81*cos(q6)*cos(q2 + q3 + q4);
             reg_vec(15) = -9.81*sin(q5)*sin(q2 + q3 + q4);
-            reg_vec(16) = -0.34335*sin(q5)*sin(q2 + q3 + q4) - 5.0031*cos(q2 + q3) - 0.849742200001078*cos(q2 + q3 + q4);          
+            reg_vec(16) = -0.34335*sin(q5)*sin(q2 + q3 + q4) - 5.0031*cos(q2 + q3)...
+                            - 0.849742200001078*cos(q2 + q3 + q4);          
         end
         
         function reg_vec = CalcRegressorJoint2(obj,q)
@@ -87,7 +160,8 @@ classdef RobotGravityIden < handle
             reg_vec(16) = 5.0031*sin(q3)*cos(q2 + q3) - 5.0031*sin(q4)*sin(q2 + q3 + q4)...
                                 - 0.849742200001078*sin(q5)^2*cos(q2 + q3 + q4)...
                                 - 0.34335*sin(q5)*sin(q2 + q3 + q4) - 0.050031*sin(q2 + q3)*cos(q3)...
-                                - 5.0031*cos(q4)*cos(q2 + q3 + q4) - 0.849742200001078*cos(q5)^2*cos(q2 + q3 + q4);
+                                - 5.0031*cos(q4)*cos(q2 + q3 + q4)...
+                                - 0.849742200001078*cos(q5)^2*cos(q2 + q3 + q4);
         end
         
         function tau_grav = GenerateGravTau(obj,jpos)
