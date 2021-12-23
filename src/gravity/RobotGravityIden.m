542a0ab828eefcf4bd88bc45818cc23751256510
zxliao <zhixiangleo@163.com>
2021-12-23 17:20:30 +0800

    fix：针对不同连杆参数建立其重力模型

--- src/gravity/RobotGravityIden.m
+++ src/gravity/RobotGravityIden.m
@@ -6,12 +6,19 @@ classdef RobotGravityIden < handle
 %         barycenter_j3 %% l3x,l3y,l4x,l4y,m4,l5x,l5y,l5z,m5,l6x,l6y,l6z,m6
 %         barycenter_j2 %% l2x,l2y,l3x,l3y,m3,l4x,l4y,m4,l5x,l5y,l5z,m5,l6x,l6y,l6z,m6
         barycenter_params
+        rbtdef
+        g
     end
     
     
     methods
-        function obj = RobotGravityIden()
-            
+        function obj = RobotGravityIden(rbtdef)
+            obj.rbtdef = rbtdef;
+            obj.g = abs(rbtdef.gravity(3));
+        end
+        
+        function LoadGravityParams(obj,file)
+            obj.barycenter_params = load(file);
         end
         
         function Identification(obj,jpos,jtau)
@@ -94,7 +101,7 @@ classdef RobotGravityIden < handle
             reg_vec(13) = -9.81*sin(q5)*cos(q6)*cos(q2 + q3 + q4);
             reg_vec(14) = 9.81*sin(q5)*sin(q6)*cos(q2 + q3 + q4);
             reg_vec(15) = 9.81*cos(q5)*cos(q2 + q3 + q4);
-            reg_vec(16) = 0.34335*cos(q5)*cos(q2 + q3 + q4);
+            reg_vec(16) = obj.g*obj.rbtdef.d(6)*cos(q5)*cos(q2 + q3 + q4);
         end
         
         function reg_vec = CalcRegressorJoint4(obj,q)
@@ -105,11 +112,12 @@ classdef RobotGravityIden < handle
             reg_vec(9) = -9.81*sin(q2 + q3 + q4)*cos(q5);
             reg_vec(10) = 9.81*sin(q5)*sin(q2 + q3 + q4);
             reg_vec(11) = -9.81*cos(q2 + q3 + q4);
-            reg_vec(12) = -0.849742200001078*cos(q2 + q3 + q4);
+            reg_vec(12) = -obj.g*obj.rbtdef.d(5)*cos(q2 + q3 + q4);
             reg_vec(13) = -9.81*sin(q6)*cos(q2 + q3 + q4) - 9.81*sin(q2 + q3 + q4)*cos(q5)*cos(q6);
             reg_vec(14) = 9.81*sin(q6)*sin(q2 + q3 + q4)*cos(q5) - 9.81*cos(q6)*cos(q2 + q3 + q4);
             reg_vec(15) = -9.81*sin(q5)*sin(q2 + q3 + q4);
-            reg_vec(16) = -0.34335*sin(q5)*sin(q2 + q3 + q4) - 0.849742200001078*cos(q2 + q3 + q4);          
+            reg_vec(16) = -obj.g*obj.rbtdef.d(6)*sin(q5)*sin(q2 + q3 + q4)... 
+                                - obj.g*obj.rbtdef.d(5)*cos(q2 + q3 + q4);          
         end
         
         function reg_vec = CalcRegressorJoint3(obj,q)
@@ -119,16 +127,18 @@ classdef RobotGravityIden < handle
             reg_vec(4) = 9.81*sin(q2 + q3);
             reg_vec(6) = -9.81*sin(q2 + q3 + q4);
             reg_vec(7) = -9.81*cos(q2 + q3 + q4);
-            reg_vec(8) = -5.0031*cos(q2 + q3);
+            reg_vec(8) = -obj.g*obj.rbtdef.a(4)*cos(q2 + q3);
             reg_vec(9) = -9.81*sin(q2 + q3 + q4)*cos(q5);
             reg_vec(10) = 9.81*sin(q5)*sin(q2 + q3 + q4);
             reg_vec(11) = -9.81*cos(q2 + q3 + q4);
-            reg_vec(12) = -5.0031*cos(q2 + q3) - 0.849742200001078*cos(q2 + q3 + q4);
+            reg_vec(12) = -obj.g*obj.rbtdef.a(4)*cos(q2 + q3)...
+                                - obj.g*obj.rbtdef.d(5)*cos(q2 + q3 + q4);
             reg_vec(13) = -9.81*sin(q6)*cos(q2 + q3 + q4) - 9.81*sin(q2 + q3 + q4)*cos(q5)*cos(q6);
             reg_vec(14) = 9.81*sin(q6)*sin(q2 + q3 + q4)*cos(q5) - 9.81*cos(q6)*cos(q2 + q3 + q4);
             reg_vec(15) = -9.81*sin(q5)*sin(q2 + q3 + q4);
-            reg_vec(16) = -0.34335*sin(q5)*sin(q2 + q3 + q4) - 5.0031*cos(q2 + q3)...
-                            - 0.849742200001078*cos(q2 + q3 + q4);          
+            reg_vec(16) = -obj.g*obj.rbtdef.d(6)*sin(q5)*sin(q2 + q3 + q4)...
+                                - obj.g*obj.rbtdef.a(4)*cos(q2 + q3)...
+                                - obj.g*obj.rbtdef.d(5)*cos(q2 + q3 + q4);          
         end
         
         function reg_vec = CalcRegressorJoint2(obj,q)
@@ -138,18 +148,23 @@ classdef RobotGravityIden < handle
             reg_vec(2) = -9.81*cos(q2);
             reg_vec(3) = -9.81*cos(q2 + q3);
             reg_vec(4) = 9.81*sin(q2 + q3);
-            reg_vec(5) = 5.0031*sin(q3)*cos(q2 + q3) - 0.050031*sin(q2 + q3)*cos(q3);
+            reg_vec(5) = obj.g*obj.rbtdef.a(3)*sin(q3)*cos(q2 + q3)...
+                                - 0.01*obj.g*obj.rbtdef.a(3)*sin(q2 + q3)*cos(q3);
             reg_vec(6) = -9.81*sin(q2 + q3 + q4);
             reg_vec(7) = -9.81*cos(q2 + q3 + q4);
-            reg_vec(8) = 5.0031*sin(q3)*cos(q2 + q3) - 5.0031*sin(q4)*sin(q2 + q3 + q4)...
-                            - 0.050031*sin(q2 + q3)*cos(q3) - 5.0031*cos(q4)*cos(q2 + q3 + q4);
+            reg_vec(8) = obj.g*obj.rbtdef.a(3)*sin(q3)*cos(q2 + q3)...
+                                - obj.g*obj.rbtdef.a(4)*sin(q4)*sin(q2 + q3 + q4)...
+                                - 0.01*obj.g*obj.rbtdef.a(3)*sin(q2 + q3)*cos(q3)...
+                                - obj.g*obj.rbtdef.a(4)*cos(q4)*cos(q2 + q3 + q4);
             reg_vec(9) = -9.81*sin(q2 + q3 + q4)*cos(q5);
             reg_vec(10) = 9.81*sin(q5)*sin(q2 + q3 + q4);
             reg_vec(11) = -9.81*sin(q5)^2*cos(q2 + q3 + q4) - 9.81*cos(q5)^2*cos(q2 + q3 + q4);
-            reg_vec(12) = 5.0031*sin(q3)*cos(q2 + q3) - 5.0031*sin(q4)*sin(q2 + q3 + q4)...
-                                - 0.849742200001078*sin(q5)^2*cos(q2 + q3 + q4)...
-                                - 0.050031*sin(q2 + q3)*cos(q3) - 5.0031*cos(q4)*cos(q2 + q3 + q4)...
-                                - 0.849742200001078*cos(q5)^2*cos(q2 + q3 + q4);
+            reg_vec(12) = obj.g*obj.rbtdef.a(3)*sin(q3)*cos(q2 + q3)...
+                                - obj.g*obj.rbtdef.a(4)*sin(q4)*sin(q2 + q3 + q4)...
+                                - obj.g*obj.rbtdef.d(5)*sin(q5)^2*cos(q2 + q3 + q4)...
+                                - 0.01*obj.g*obj.rbtdef.a(3)*sin(q2 + q3)*cos(q3)...
+                                - obj.g*obj.rbtdef.a(4)*cos(q4)*cos(q2 + q3 + q4)...
+                                - obj.g*obj.rbtdef.d(5)*cos(q5)^2*cos(q2 + q3 + q4);
             reg_vec(13) = -9.81*sin(q5)^2*sin(q6)*cos(q2 + q3 + q4)...
                                 - 9.81*sin(q6)*cos(q5)^2*cos(q2 + q3 + q4)...
                                 - 9.81*sin(q2 + q3 + q4)*cos(q5)*cos(q6);
@@ -157,11 +172,13 @@ classdef RobotGravityIden < handle
                                 + 9.81*sin(q6)*sin(q2 + q3 + q4)*cos(q5)...
                                  - 9.81*cos(q5)^2*cos(q6)*cos(q2 + q3 + q4);
             reg_vec(15) = -9.81*sin(q5)*sin(q2 + q3 + q4);
-            reg_vec(16) = 5.0031*sin(q3)*cos(q2 + q3) - 5.0031*sin(q4)*sin(q2 + q3 + q4)...
-                                - 0.849742200001078*sin(q5)^2*cos(q2 + q3 + q4)...
-                                - 0.34335*sin(q5)*sin(q2 + q3 + q4) - 0.050031*sin(q2 + q3)*cos(q3)...
-                                - 5.0031*cos(q4)*cos(q2 + q3 + q4)...
-                                - 0.849742200001078*cos(q5)^2*cos(q2 + q3 + q4);
+            reg_vec(16) = obj.g*obj.rbtdef.a(3)*sin(q3)*cos(q2 + q3)...
+                                - obj.g*obj.rbtdef.a(4)*sin(q4)*sin(q2 + q3 + q4)...
+                                - obj.g*obj.rbtdef.d(5)*sin(q5)^2*cos(q2 + q3 + q4)...
+                                - obj.g*obj.rbtdef.d(6)*sin(q5)*sin(q2 + q3 + q4)...
+                                - 0.01*obj.g*obj.rbtdef.a(3)*sin(q2 + q3)*cos(q3)...
+                                - obj.g*obj.rbtdef.a(4)*cos(q4)*cos(q2 + q3 + q4)...
+                                - obj.g*obj.rbtdef.d(5)*cos(q5)^2*cos(q2 + q3 + q4);
         end
         
         function tau_grav = GenerateGravTau(obj,jpos)
