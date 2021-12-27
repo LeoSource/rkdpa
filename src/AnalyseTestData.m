bd33be752c4679215a6dddf9229fc6c0b3e09c84
zxliao <zhixiangleo@163.com>
2021-12-27 18:54:19 +0800

    feat：增加力矩对比查看功能

--- src/AnalyseTestData.m
+++ src/AnalyseTestData.m
@@ -7,14 +7,14 @@ addpath(genpath('tools'));
 dt = 0.005;
 %% analyse test data
 nj = 6;
-td = load('./data/test_data_1215_174740.csv');
+td = load('./data/test_data_1227_141827_toilet.csv');
 jpos_idx = 1; jvel_idx = 2; jtor_idx = 3;
 jpos = td(:,1:jpos_idx*nj);
 jvel = td(:,nj+1:jvel_idx*nj);
 jtor = td(:,2*nj+1:jtor_idx*nj);
 t = 0:dt:dt*(size(td,1)-1);
 
-analysis_mode = 'filter';
+analysis_mode = 'tau_compare';
 switch analysis_mode
     case 'common'
 %% plot joint position, velocity and torque
@@ -71,6 +71,7 @@ for idx=1:6
 end
 
     case 'collision'
+%% collision detector
 fs = 200;
 fc = 10;
 for idx=1:6
@@ -89,5 +90,46 @@ for idx=1:6
     ylabel('joint torque bias(Nm)');
 end
 
+    case 'tau_compare'
+%% analyse joint torque during clean task
+rbtdef = CreateRobot();
+rbtdyn = RobotDynamics(rbtdef);
+rbtdyn.LoadParams('gravity/gravity_parameters.txt','gravity/friction_parameters.txt');
+fs = 200;
+fc = 10;
+for idx=1:6
+    tau_filter = Biquad('LOWPASS', fc/fs, 0.707, 0);
+    for nidx=1:length(jtor(:,idx))
+        tor(idx,nidx) = tau_filter.Filter(jtor(nidx,idx));
+    end
+end
+for nidx=1:length(jtor(:,1))
+    tau_iden(:,nidx) = rbtdyn.GenerateIdenTorque(jpos(nidx,:),jvel(nidx,:));
+end
+for idx=1:6
+    figure
+    plot(t,tau_iden(idx,:), t,tor(idx,:)); grid on;
+    title(['joint',num2str(idx)]);
+    xlabel('time(s)'); ylabel('joint torque(Nm)');
 end
 
+end
+
+
+%% robot description
+function rbt = CreateRobot()
+    d1 = 0.048; a2 = 0.51; a3 = 0.51;
+    d4 = 0.11; d5 = 0.08662; d6 = 0.035;
+    mdh_table = [0, d1, 0, 0, 0, 0;...
+                        0, 0, 0, -pi/2, 0, -pi/2;...
+                        0, 0, a2, 0, 0, pi/2;...
+                        0, d4, a3, 0, 0, -pi/2;...
+                        0, d5, 0, -pi/2, 0, 0;...
+                        0, d6, 0, pi/2, 0, 0];
+    % pose_tool = SE3(rotx(-10), [0,0,0.116]);
+    tool_toiletlid = SE3(rotx(0), [0,-0.035,0.23]);
+    qmin = [-pi, -pi/2, -4*pi/3, -pi, -pi, -2*pi]';
+    qmax = [pi, pi/2, pi/3, pi, pi, 2*pi]';
+    rbt = SerialLink(mdh_table, 'modified', 'name', 'CleanRobot', 'tool',tool_toiletlid);
+    rbt.qlim(:,1) = qmin; rbt.qlim(:,2) = qmax;
+end
